7a062764cf3e008b7c78867b57b8e239f9922d42
Haesung <koh1260@naver.com>
2024-12-31 23:26:38 +0900

    refactor: 이미지 base url 상수로 관리

--- src/presentation/controllers/user/users.controller.ts
+++ src/presentation/controllers/user/users.controller.ts
@@ -5,20 +5,16 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ConfigService } from '@nestjs/config';
 import { CreateProfileImageMulterOptions } from '../../../configs/multer-s3/multer-options';
 import { UploadProfileImageResponse } from '../../dto/user/upload-image.response';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly config: ConfigService) {}
   @UseInterceptors(FileInterceptor('file', CreateProfileImageMulterOptions()))
   @Post('profile/image')
   uploadProfileImage(
     @UploadedFile() file: Express.MulterS3.File,
   ): UploadProfileImageResponse {
-    return {
-      imageUrl: `${this.config.get<string>('CDN_PREFIX')}/${file.key}`,
-    };
+    return UploadProfileImageResponse.create(file.key);
   }
 }
--- src/presentation/dto/user/upload-image.response.ts
+++ src/presentation/dto/user/upload-image.response.ts
@@ -1,3 +1,13 @@
+import { IMAGE_BASE_URL } from '../../../common/constant';
+
 export class UploadProfileImageResponse {
   readonly imageUrl: string;
+
+  constructor(imagePath: string) {
+    this.imageUrl = `${IMAGE_BASE_URL}/${imagePath}`;
+  }
+
+  static create(imagePath: string) {
+    return new UploadProfileImageResponse(imagePath);
+  }
 }

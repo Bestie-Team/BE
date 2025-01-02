06b92f4a389244a2329670c3e34ca3be0d33e68a
Haesung <koh1260@naver.com>
2025-01-02 17:13:32 +0900

    feat: 로그인 응답 dto 추가

--- src/presentation/controllers/auth/auth.controller.ts
+++ src/presentation/controllers/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { LoginRequest } from '../../dto/auth/login.request';
 import { AuthService } from '../../../domain/services/auth/auth.service';
 import { Provider } from '../../../shared/types/index';
 import { ValidateProviderPipe } from '../../../common/pipes/validate-provider.pipe';
+import { LoginResponse } from '../../dto/auth/login.response';
 
 @Controller('auth')
 export class AuthController {
@@ -12,8 +13,8 @@ export class AuthController {
   async login(
     @Param('provider', ValidateProviderPipe) provider: Provider,
     @Body() dto: LoginRequest,
-  ) {
+  ): Promise<LoginResponse> {
     const { accessToken } = dto;
-    await this.authService.login(provider, accessToken);
+    return await this.authService.login(provider, accessToken);
   }
 }

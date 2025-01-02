a76c258422f620b4e97b1b49b2e6bd133a0d283b
Haesung <koh1260@naver.com>
2025-01-02 18:42:26 +0900

    feat: auth 가드 부착

--- src/presentation/controllers/user/users.controller.ts
+++ src/presentation/controllers/user/users.controller.ts
@@ -2,12 +2,15 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateProfileImageMulterOptions } from '../../../configs/multer-s3/multer-options';
 import { UploadProfileImageResponse } from '../../dto/user/upload-image.response';
+import { AuthGuard } from '../../../common/guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UsersController {
   @UseInterceptors(FileInterceptor('file', CreateProfileImageMulterOptions()))

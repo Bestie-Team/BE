84d5af9add529cc46bb226a199df63f2c34ab302
Haesung <koh1260@naver.com>
2025-01-06 20:42:57 +0900

    feat: 친구 요청 커서 get 함수 구현, 커서 타입 변경

--- src/domain/shared/get-cursor.ts
+++ src/domain/shared/get-cursor.ts
@@ -1,5 +1,23 @@
+import { FriendRequest } from 'src/domain/types/friend.types';
 import { User } from 'src/domain/types/user.types';
 
-export const getUserCursor = (users: User[], limit: number): string | null => {
-  return users[limit - 1]?.name || null;
+export const getUserCursor = (users: User[], limit: number) => {
+  return users[limit - 1]
+    ? {
+        name: users[limit - 1].name,
+        accountId: users[limit - 1].accountId,
+      }
+    : null;
+};
+
+export const getFriendCursor = (
+  friendRequests: FriendRequest[],
+  limit: number,
+) => {
+  return friendRequests[limit - 1]
+    ? {
+        name: friendRequests[limit - 1].sender.name,
+        accountId: friendRequests[limit - 1].sender.accountId,
+      }
+    : null;
 };

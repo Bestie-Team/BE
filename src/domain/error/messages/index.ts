04677502594280019cdd57e36bf874447d4d5582
Haesung <koh1260@naver.com>
2025-01-07 22:54:34 +0900

    feat: 친구 관계가 아닌 경우 에러 메세지 정의

--- src/domain/error/messages/index.ts
+++ src/domain/error/messages/index.ts
@@ -4,3 +4,4 @@ export const NOT_FOUND_USER_MESSAGE = (info: string) =>
 export const FRIEND_REQUEST_ALREADY_EXIST_MESSAGE = `이미 존재하는 요청입니다.`;
 export const FORBIDDEN_MESSAGE = '권한이 없습니다.';
 export const NOT_FOUND_FRIEND_MESSAGE = '존재하지 않는 친구 혹은 요청입니다.';
+export const IS_NOT_FRIEND_RELATION_MESSAGE = '친구 관계가 아닙니다.';

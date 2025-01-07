0443a9edb4da1109b15c9ac500e0a338ebf958e9
Haesung <koh1260@naver.com>
2025-01-08 00:53:02 +0900

    docs: 그룹 생성 swagger 등록

--- src/presentation/controllers/group/groups.controller.ts
+++ src/presentation/controllers/group/groups.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { GroupCreateService } from 'src/domain/services/group/group-create.service';
@@ -11,6 +11,18 @@ import { CreateGroupRequest } from 'src/presentation/dto/group/request/create-gr
 export class GroupsController {
   constructor(private readonly groupsCreateService: GroupCreateService) {}
 
+  @ApiOperation({ summary: '그룹 생성' })
+  @ApiBody({
+    type: CreateGroupRequest,
+  })
+  @ApiResponse({
+    status: 201,
+    description: '그룹 생성 완료',
+  })
+  @ApiResponse({
+    status: 400,
+    description: '입력값 검증 실패, friendIds에 친구가 아닌 회원이 존재할 경우',
+  })
   @Post()
   async create(@Body() dto: CreateGroupRequest, @CurrentUser() userId: string) {
     const { friendIds, ...rest } = dto;

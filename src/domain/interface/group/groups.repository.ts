8dc85c68f0748f34f93db1690b6f2a5ec56dac07
Haesung <koh1260@naver.com>
2025-01-10 01:41:32 +0900

    feat: 그룹 목록 검색 repo 메서드 구현

--- src/domain/interface/group/groups.repository.ts
+++ src/domain/interface/group/groups.repository.ts
@@ -1,7 +1,13 @@
 import { GroupEntity } from 'src/domain/entities/group/group.entity';
+import { Group } from 'src/domain/types/group.types';
+import { PaginationInput } from 'src/shared/types';
 
 export interface GroupsRepository {
   save(data: GroupEntity): Promise<void>;
+  findGroupsByUserId(
+    userId: string,
+    paginationInput: PaginationInput,
+  ): Promise<Group[]>;
 }
 
 export const GroupsRepository = Symbol('GroupsRepository');

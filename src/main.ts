d9650c38fbdb5414dc9dcf5eaa88b95e15c6a195
Haesung <koh1260@naver.com>
2025-01-10 21:09:43 +0900

    feat: 기본 로거 winston으로 변경 및 로그 파일 저장

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from 'src/configs/swagger/setup-swagger';
+import { winstonLogger } from 'src/configs/winston/winston-options';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+  });
   setupSwagger(app);
   await app.listen(8080);
 }

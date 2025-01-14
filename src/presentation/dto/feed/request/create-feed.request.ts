c8a01ec7acb47300b281c8f2ac1b5c2f6bb842aa
Haesung <koh1260@naver.com>
2025-01-14 22:20:51 +0900

    feat: 피드 생성 유효성 검사 추가

--- src/presentation/dto/feed/request/create-feed.request.ts
+++ src/presentation/dto/feed/request/create-feed.request.ts
@@ -3,9 +3,11 @@ import {
   ArrayMaxSize,
   ArrayNotEmpty,
   IsArray,
+  IsNotEmpty,
   IsUrl,
   IsUUID,
   Length,
+  ValidateIf,
 } from 'class-validator';
 
 export class CreateFeedRequest {
@@ -14,12 +16,14 @@ export class CreateFeedRequest {
     type: 'string',
     nullable: true,
   })
-  @IsUUID(4, { each: true, message: '그룹 번호 형식이 맞지 않습니다.' })
+  @IsUUID(4, { each: true, message: '그룹 번호는 UUID여야 합니다.' })
+  @IsNotEmpty({ message: '그룹 번호가 없다면 null을 넣어주세요.' })
+  @ValidateIf((_, value) => value !== null)
   readonly gatheringId: string | null;
 
   @ApiProperty({ type: [String] })
   @IsArray()
-  @IsUrl({}, { each: true, message: '친구 번호 형식이 맞지 않습니다.' })
+  @IsUrl({}, { each: true, message: '친구 번호는 UUID여야 합니다.' })
   @ArrayNotEmpty({ message: '피드 이미지는 1장 이상이어야 합니다.' })
   @ArrayMaxSize(5, {
     message: '피드 이미지는 최대 5장까지 가능합니다 가능합니다.',
@@ -27,6 +31,6 @@ export class CreateFeedRequest {
   readonly imageUrls: string[];
 
   @ApiProperty()
-  @Length(20, 150)
+  @Length(20, 150, { message: '피드 내용은 20 ~ 150자만 가능합니다.' })
   readonly content: string;
 }

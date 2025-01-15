1216a5cb440623174aa0af503d2260ec3fbfe531
Haesung <koh1260@naver.com>
2025-01-15 21:29:08 +0900

    chore: dto 타입 버전업 - 모임 관련 타입 추가

---
+++ src/presentation/dto/gathering/index.ts
@@ -0,0 +1,7 @@
+export * from './request/create-gathering.request';
+export * from './request/gathering-invitation-list.request';
+export * from './request/gathering-list.request';
+
+export * from './response/gathering-detail.response';
+export * from './response/gathering-invitation-list.response';
+export * from './response/gathering-list.response';

61dc5186aabf672082bfbff240cfe6af1995aead
Haesung <koh1260@naver.com>
2025-01-15 21:14:40 +0900

    fix: 누락된 Injectable 부착

--- src/infrastructure/repositories/feed/friend-feed-visibilities-prisma.repository.ts
+++ src/infrastructure/repositories/feed/friend-feed-visibilities-prisma.repository.ts
@@ -1,8 +1,10 @@
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { Injectable } from '@nestjs/common';
 import { FriendFeedVisibilityEntity } from 'src/domain/entities/feed/friend-feed-visibility.entity';
 import { FriendFeedVisibilitiesRepository } from 'src/domain/interface/feed/friend-feed-visibilities.repository';
 
+@Injectable()
 export class FriendFeedVisibilitiesPrismaRepository
   implements FriendFeedVisibilitiesRepository
 {

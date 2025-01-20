2b069cdbe98263c54be7cdbd7711a0e2de2b8347
Haesung <koh1260@naver.com>
2025-01-21 00:42:05 +0900

    fix: 모임 참여 repo 순환 의존 해결을 위해 모듈 분리

--- src/modules/gathering/gatherings.module.ts
+++ src/modules/gathering/gatherings.module.ts
@@ -1,27 +1,22 @@
 import { Module } from '@nestjs/common';
-import { GatheringParticipationsRepository } from 'src/domain/interface/gathering/gathering-participations.repository';
 import { GatheringsRepository } from 'src/domain/interface/gathering/gatherings.repository';
 import { GatheringInvitationsReadService } from 'src/domain/services/gathering/gathering-invitations-read.service';
 import { GatheringsReadService } from 'src/domain/services/gathering/gatherings-read.service';
 import { GatheringsWriteService } from 'src/domain/services/gathering/gatherings-write.service';
-import { GatheringParticipationsPrismaRepository } from 'src/infrastructure/repositories/gathering/gathering-participations-prisma.repository';
 import { GatheringsPrismaRepository } from 'src/infrastructure/repositories/gathering/gatherings-prisma.repository';
 import { FriendsModule } from 'src/modules/friend/friends.module';
+import { GatheringParticipationModules } from 'src/modules/gathering/gathering-participation.module';
 import { GroupsModule } from 'src/modules/group/groups.module';
 import { GatheringsController } from 'src/presentation/controllers/gathering/gatherings.controller';
 
 @Module({
-  imports: [FriendsModule, GroupsModule],
+  imports: [FriendsModule, GroupsModule, GatheringParticipationModules],
   controllers: [GatheringsController],
   providers: [
     GatheringsWriteService,
     GatheringsReadService,
     GatheringInvitationsReadService,
     { provide: GatheringsRepository, useClass: GatheringsPrismaRepository },
-    {
-      provide: GatheringParticipationsRepository,
-      useClass: GatheringParticipationsPrismaRepository,
-    },
   ],
   exports: [
     { provide: GatheringsRepository, useClass: GatheringsPrismaRepository },

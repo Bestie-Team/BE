14bb05172bde9b3008f5f261407435f765d75e25
Haesung <koh1260@naver.com>
2025-02-01 18:23:16 +0900

    feat: 카카오 로그인 구현

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,6 +1,8 @@
 export const IMAGE_BASE_URL = 'https://cdn.lighty.today';
 export const GOOGLE_USER_INFO_URL =
   'https://www.googleapis.com/oauth2/v3/userinfo';
+export const KAKAO_USER_INFO_URL =
+  'https://kapi.kakao.com/v2/user/me?secure_resource=true';
 
 export const notificationTypes = {
   GATHERING_INVITATION_RECEIVED: 'GATHERING_INVITATION_RECEIVED',
--- src/infrastructure/auth/context/oauth-context.ts
+++ src/infrastructure/auth/context/oauth-context.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { Provider } from 'src/shared/types';
 import { GoogleStrategy } from '../strategies/google-strategy';
+import { KakaoStrategy } from 'src/infrastructure/auth/strategies/kakao-strategy';
+import { OauthUserInfo } from 'src/infrastructure/auth/strategies/oauth-strategy';
 @Injectable()
 export class OauthContext {
-  constructor(private readonly googleStrategy: GoogleStrategy) {}
+  constructor(
+    private readonly googleStrategy: GoogleStrategy,
+    private readonly kakaoStrategy: KakaoStrategy,
+  ) {}
 
-  async getUserInfo(provider: Provider, token: string) {
+  async getUserInfo(provider: Provider, token: string): Promise<OauthUserInfo> {
     // provider 별 구현체 추가되면 의존성 및 조건문 추가
-    return await this.googleStrategy.getUserInfo(token);
+    return provider === 'GOOGLE'
+      ? await this.googleStrategy.getUserInfo(token)
+      : provider === 'KAKAO'
+      ? await this.kakaoStrategy.getUserInfo(token)
+      : {
+          email: 'apple-not-imple',
+          name: 'name',
+          provider: 'APPLE',
+        };
   }
 }

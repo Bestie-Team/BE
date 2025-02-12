c177835ae938f6d23e9f7b9f2601b156ef185434
Haesung <koh1260@naver.com>
2025-02-13 00:41:55 +0900

    chore: 피드 업데이트 최소 글자수 1자로 변경

--- src/presentation/dto/feed/request/update-feed.request.ts
+++ src/presentation/dto/feed/request/update-feed.request.ts
@@ -3,6 +3,6 @@ import { Length } from 'class-validator';
 
 export class UpdateFeedRequest {
   @ApiProperty()
-  @Length(20, 150, { message: '피드 내용은 20 ~ 150자만 가능합니다.' })
+  @Length(1, 150, { message: '피드 내용은 1 ~ 150자만 가능합니다.' })
   readonly content: string;
 }

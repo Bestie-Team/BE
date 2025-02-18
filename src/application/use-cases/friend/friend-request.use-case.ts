209ae3c279e005f8c09b42c228f442705d64221d
Haesung <koh1260@naver.com>
2025-02-19 03:33:33 +0900

    refactor: 친구 요청 use case에 event emitter 의존성 제거 및 변경된 알림 create 사용

--- src/application/use-cases/friend/friend-request.use-case.ts
+++ src/application/use-cases/friend/friend-request.use-case.ts
@@ -1,13 +1,9 @@
-import { Transactional } from '@nestjs-cls/transactional';
 import { Injectable } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
 import { APP_NAME } from 'src/common/constant';
 import { FriendWriteService } from 'src/domain/services/friend/friend-write.service';
 import { NotificationsService } from 'src/domain/services/notification/notifications.service';
 import { UsersService } from 'src/domain/services/user/users.service';
 import { FriendPrototype } from 'src/domain/types/friend.types';
-import { NotificationPrototype } from 'src/domain/types/notification.types';
-import { NotificationPayload } from 'src/infrastructure/types/notification.types';
 
 @Injectable()
 export class FriendRequestUseCase {
@@ -15,47 +11,32 @@ export class FriendRequestUseCase {
     private readonly friendWriteService: FriendWriteService,
     private readonly notificationService: NotificationsService,
     private readonly usersService: UsersService,
-    private readonly eventEmitter: EventEmitter2,
   ) {}
 
   async execute(input: FriendPrototype) {
     const { receiverId, senderId } = input;
-    const receiver = await this.usersService.getUserByIdOrThrow(receiverId);
-    const sender = await this.usersService.getUserByIdOrThrow(senderId);
-    const message = this.generateNotificationMessage(sender.name);
 
     await this.friendWriteService.checkExistFriend(senderId, receiverId);
-    await this.transaction(input, {
-      message,
-      title: APP_NAME,
-      type: 'FRIEND_REQUEST',
-      userId: receiverId,
-      relatedId: null,
-    });
+    await this.friendWriteService.request(input);
+
+    this.notify(senderId, receiverId);
+  }
+
+  async notify(senderId: string, receiverId: string) {
+    const receiver = await this.usersService.getUserByIdOrThrow(receiverId);
 
     if (receiver.notificationToken && receiver.serviceNotificationConsent) {
-      this.publishNotification({
-        token: receiver.notificationToken,
+      const sender = await this.usersService.getUserByIdOrThrow(senderId);
+      const message = `${sender.name}님이 친구 요청을 보냈어요!`;
+
+      this.notificationService.createV2({
+        message,
+        type: 'FRIEND_REQUEST',
         title: APP_NAME,
-        body: message,
+        userId: receiver.id,
+        token: receiver.notificationToken,
+        relatedId: null,
       });
     }
-  }
-
-  @Transactional()
-  private async transaction(
-    friendInput: FriendPrototype,
-    notificationInput: NotificationPrototype,
-  ) {
-    await this.friendWriteService.request(friendInput);
-    await this.notificationService.create(notificationInput);
-  }
-
-  private generateNotificationMessage(receiverName: string): string {
-    return `${receiverName}님이 친구 요청을 보냈어요.`;
-  }
-
-  private async publishNotification(payload: NotificationPayload) {
-    this.eventEmitter.emit('notify', payload);
   }
 }

dc3230812b47737c7999da4562b7d80224b08525
Haesung <koh1260@naver.com>
2025-02-21 20:11:32 +0900

    chore: dto 타입 버전업 - 친구 수락 요청 dto 추가

--- src/presentation/dto/friend/index.ts
+++ src/presentation/dto/friend/index.ts
@@ -1,4 +1,6 @@
 export * from './request/create-friend.request';
 export * from './request/search-friend.request';
+export * from './request/accept-friend.request';
+
 export * from './response/friend-list.response';
 export * from './response/friend-request-list.response';

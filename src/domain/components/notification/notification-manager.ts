6c9fdf3fed941426909e3f8104e991f973c98c9c
Haesung <koh1260@naver.com>
2025-02-24 02:55:29 +0900

    feat: 알림 생성 전송만을 수행하는 notification manager 구현

---
+++ src/domain/components/notification/notification-manager.ts
@@ -0,0 +1,58 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { v4 } from 'uuid';
+import { NotificationEntity } from 'src/domain/entities/notification/notification.entity';
+import { NotificationsRepository } from 'src/domain/interface/notification/notifications.repository';
+import { NotificationPrototype } from 'src/domain/types/notification.types';
+import { EventPublisher } from 'src/infrastructure/event/publishers/interface/event-publisher';
+import { UsersReader } from 'src/domain/components/user/users-reader';
+import { APP_NAME } from 'src/common/constant';
+
+@Injectable()
+export class NotificationsManager {
+  private readonly logger = new Logger('NotificationsManager');
+
+  constructor(
+    @Inject(NotificationsRepository)
+    private readonly notificationRepository: NotificationsRepository,
+    @Inject(EventPublisher)
+    private readonly eventPublisher: EventPublisher,
+    private readonly usersReader: UsersReader,
+  ) {}
+
+  async create(input: NotificationPrototype & { token: string }) {
+    const stdDate = new Date();
+    const notification = NotificationEntity.create(input, v4, stdDate);
+    this.notificationRepository.save(notification);
+    this.eventPublisher.publish('notify', {
+      body: input.message,
+      title: input.title,
+      token: input.token,
+    });
+  }
+
+  async sendGatheringCreation(senderId: string, receiverIds: string[]) {
+    const sender = await this.usersReader.readOne(senderId);
+    const receivers = await this.usersReader.readMulti(receiverIds);
+
+    const notificationPromises = receivers.map(async (receiver) => {
+      if (receiver.notificationToken && receiver.serviceNotificationConsent) {
+        return this.create({
+          message: `${sender.name}님이 약속 초대장을 보냈어요!`,
+          type: 'GATHERING_INVITATION_RECEIVED',
+          title: APP_NAME,
+          userId: receiver.id,
+          token: receiver.notificationToken,
+          relatedId: null,
+        }).catch((e: Error) =>
+          this.logger.log({
+            message: `알림 에러: ${e.message}`,
+            stack: e.stack,
+            timestamp: new Date().toISOString(),
+          }),
+        );
+      }
+    });
+
+    Promise.all(notificationPromises);
+  }
+}
--- src/modules/notification/notifications.module.ts
+++ src/modules/notification/notifications.module.ts
@@ -4,17 +4,20 @@ import { NotificationsService } from 'src/domain/components/notification/notific
 import { EventModule } from 'src/infrastructure/event/event.module';
 import { NotificationsPrismaRepository } from 'src/infrastructure/repositories/notification/notifications-prisma.repository';
 import { NotificationsController } from 'src/presentation/controllers/notification/notifications.controller';
+import { NotificationsManager } from 'src/domain/components/notification/notification-manager';
+import { UsersModule } from 'src/modules/user/users.module';
 
 @Module({
-  imports: [EventModule],
+  imports: [EventModule, UsersModule],
   controllers: [NotificationsController],
   providers: [
     NotificationsService,
+    NotificationsManager,
     {
       provide: NotificationsRepository,
       useClass: NotificationsPrismaRepository,
     },
   ],
-  exports: [NotificationsService],
+  exports: [NotificationsService, NotificationsManager],
 })
 export class NotificationsModule {}

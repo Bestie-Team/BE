ba9a4905fc333b9865815cc5f4654742c17fe083
Haesung <koh1260@naver.com>
2025-02-24 19:52:54 +0900

    refactor: group component module 분리

--- src/modules/group/groups.module.ts
+++ src/modules/group/groups.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GroupCreationUseCase } from 'src/application/use-cases/group/group-creation.use-case';
 import { GroupParticipationsRepository } from 'src/domain/interface/group/group-participations.repository';
-import { GroupsRepository } from 'src/domain/interface/group/groups.repository';
-import { GroupsWriter } from 'src/domain/components/group/groups-writer';
-import { GroupsReader } from 'src/domain/components/group/groups-reader';
 import { GroupParticipationsPrismaRepository } from 'src/infrastructure/repositories/group/group-participations-prisma.repository';
-import { GroupsPrismaRepository } from 'src/infrastructure/repositories/group/groups-prisma.repository';
 import { NotificationsModule } from 'src/modules/notification/notifications.module';
 import { UsersModule } from 'src/modules/user/users.module';
 import { GroupsController } from 'src/presentation/controllers/group/groups.controller';
@@ -13,27 +9,28 @@ import { FriendsCheckerModule } from 'src/modules/friend/friends-chcker.module';
 import { GroupsService } from 'src/domain/services/groups/groups.service';
 import { GroupParticipationsWriter } from 'src/domain/components/group/group-participations-writer';
 import { GroupParticipationsReader } from 'src/domain/components/group/group-participations-reader';
+import { GroupsComponentModule } from 'src/modules/group/groups-component.module';
 
 @Module({
-  imports: [UsersModule, NotificationsModule, FriendsCheckerModule],
+  imports: [
+    GroupsComponentModule,
+    UsersModule,
+    NotificationsModule,
+    FriendsCheckerModule,
+  ],
   controllers: [GroupsController],
   providers: [
     GroupCreationUseCase,
-    GroupsWriter,
-    GroupsReader,
     GroupsService,
     GroupParticipationsWriter,
     GroupParticipationsReader,
-    { provide: GroupsRepository, useClass: GroupsPrismaRepository },
     {
       provide: GroupParticipationsRepository,
       useClass: GroupParticipationsPrismaRepository,
     },
   ],
   exports: [
-    GroupsReader,
     GroupParticipationsReader,
-    { provide: GroupsRepository, useClass: GroupsPrismaRepository },
     {
       provide: GroupParticipationsRepository,
       useClass: GroupParticipationsPrismaRepository,

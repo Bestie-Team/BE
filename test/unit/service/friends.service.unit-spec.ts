62757f3427bde07801bf4b8628b570fd3d9d2170
Haesung <koh1260@naver.com>
2025-02-26 17:51:52 +0900

    test: 친구 요청 수락 예외 테스트 작성
    - 존재하지 않는 대상
    - 이미 친구인 회원 대상

--- test/unit/service/friends.service.unit-spec.ts
+++ test/unit/service/friends.service.unit-spec.ts
@@ -6,6 +6,7 @@ import {
   FRIEND_ALREADY_EXIST_MESSAGE,
   FRIEND_REQUEST_ALREADY_EXIST_MESSAGE,
   IS_NOT_FRIEND_RELATION_MESSAGE,
+  NOT_FOUND_FRIEND_MESSAGE,
   NOT_FOUND_USER_MESSAGE,
 } from 'src/domain/error/messages';
 import { FriendsService } from 'src/domain/services/friends/friends.service';
@@ -114,6 +115,32 @@ describe('FriendsService', () => {
       await expect(async () =>
         friendsService.unfriend(nonExistId, sender.id),
       ).rejects.toThrow(new NotFoundException(IS_NOT_FRIEND_RELATION_MESSAGE));
+    });
+  });
+
+  describe('친구 요청 수락', () => {
+    const sender = generateUserEntity('sender@test.com', 'sender_id');
+    const receiver = generateUserEntity('receiver@test.com', 'receiver_id');
+
+    beforeEach(async () => {
+      await db.user.createMany({ data: [sender, receiver] });
+    });
+
+    it('이미 친구 관계인 회원인 경우 예외가 발생한다.', async () => {
+      const friend = generateFriendEntity(sender.id, receiver.id, 'ACCEPTED');
+      await db.friend.create({ data: friend });
+
+      await expect(async () =>
+        friendsService.accept(sender.id, receiver.id),
+      ).rejects.toThrow(new ConflictException(FRIEND_ALREADY_EXIST_MESSAGE));
+    });
+
+    it('대상이 존재하지 않는 회원이면 예외가 발생한다.', async () => {
+      const nonExistId = '0890d33b-543b-4fc7-88dc-c5eb4acf57eb';
+
+      await expect(async () =>
+        friendsService.accept(sender.id, nonExistId),
+      ).rejects.toThrow(new NotFoundException(NOT_FOUND_FRIEND_MESSAGE));
     });
   });
 });

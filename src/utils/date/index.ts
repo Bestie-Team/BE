069c029ea2fa7d96ca9e70bb37540e8b5dd096e0
Haesung <koh1260@naver.com>
2025-03-06 02:11:54 +0900

    chore: 사용하지 않는 함수 제거

--- src/utils/date/index.ts
+++ src/utils/date/index.ts
@@ -1,17 +1,3 @@
-export const calcDiff = (firstDate: Date, secondDate: Date) => {
-  return Math.abs(firstDate.getTime() - secondDate.getTime());
-};
-
-export const convertUnixToDate = (time: number) => {
-  const date = Math.floor(time / (1000 * 60 * 60 * 24));
-  return date;
-};
-
-export const calcDiffDate = (firstDate: Date, secondDate: Date) => {
-  const unixTime = Math.abs(firstDate.getTime() - secondDate.getTime());
-  return convertUnixToDate(unixTime);
-};
-
 export const calcDateDiff = (
   startDate: Date,
   endDate: Date,
